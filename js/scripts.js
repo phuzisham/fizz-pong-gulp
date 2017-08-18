d18b577509e8c98aeb62d86c2c09854f3a67ed22
Guest User <Guest@Epicodus-8A.local>
2017-08-18 14:10:20 -0700

    add reverse button to reverse output

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,31 @@ $(document).ready(function() {
       var userInput = $('#userInput').val();
       fizz(userInput);
   });
+  $('#reverse').click(function(event) {
+      event.preventDefault();
+      var userInput = $('#userInput').val();
+      reverseFizz(userInput);
+  });
 });
 
 function fizz(num) {
   $('#resultList ul').empty();
   for (var i = 1; i <= num; i++) {
+    if (i % 15 == 0) {
+      $('#resultList ul').append('<li>'+'fizz-pong'+'</li>');
+    } else if (i % 5 == 0) {
+        $('#resultList ul').append('<li>'+'pong'+'</li>');
+    } else if (i % 3 == 0) {
+        $('#resultList ul').append('<li>'+'fizz'+'</li>');
+    } else {
+        $('#resultList ul').append('<li>'+i+'</li>');
+    }
+  }
+}
+
+function reverseFizz(num) {
+  $('#resultList ul').empty();
+  for (var i = num; i >= 1; i--) {
     if (i % 15 == 0) {
       $('#resultList ul').append('<li>'+'fizz-pong'+'</li>');
     } else if (i % 5 == 0) {
