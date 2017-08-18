762cd3c4e62470332e7fdb70a6762ff36b45dd3b
Guest User <Guest@Epicodus-8A.local>
2017-08-18 11:50:53 -0700

    basic functionality and content

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-  $('#pingForm').submit(function(event) {
+  $('#fizzForm').submit(function(event) {
       event.preventDefault();
       var userInput = $('#userInput').val();
-      ping(userInput);
+      fizz(userInput);
   });
 });
 
-function ping(num) {
+function fizz(num) {
   $('#resultList ul').empty();
   for (var i = 1; i <= num; i++) {
     if (i % 15 == 0) {
-      $('#resultList ul').append('<li>'+'ping-pong'+'</li>');
+      $('#resultList ul').append('<li>'+'fizz-pong'+'</li>');
     } else if (i % 5 == 0) {
-        $('#resultList ul').append('<li>'+'ping'+'</li>');
-    } else if (i % 3 == 0) {
         $('#resultList ul').append('<li>'+'pong'+'</li>');
+    } else if (i % 3 == 0) {
+        $('#resultList ul').append('<li>'+'fizz'+'</li>');
     } else {
         $('#resultList ul').append('<li>'+i+'</li>');
     }
